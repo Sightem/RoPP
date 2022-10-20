ac7515690fa7e565ba4de59f71d76b841cb18d62
sightem <sightem@national.shitposting.agency>
2022-10-19 21:51:18 -0400

    implementations

--- RoPP/user.cpp
+++ RoPP/user.cpp
@@ -86,3 +86,95 @@ int RoPP::User::GetFollowingsCount()
 
     return json::parse(res.data)["count"];
 }
+
+/*
+* @brief gets the friends online of the user
+* @return friends online json object
+*/
+json RoPP::User::GetFriendsOnline()
+{
+    Request req("https://friends.roblox.com/v1/users/" + std::to_string(this->UID) + "/friends/online");
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    return json::parse(res.data);
+}
+
+/*
+* @brief gets the username of the user
+* @return username
+*/
+std::string RoPP::User::GetUsername()
+{
+    Request req("https://users.roblox.com/v1/users/" + std::to_string(this->UID));
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    return json::parse(res.data)["name"];
+}
+
+/*
+* @brief gets the display name of the user
+* @return display name
+*/
+std::string RoPP::User::GetDisplayName()
+{
+    Request req("https://users.roblox.com/v1/users/" + std::to_string(this->UID));
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    return json::parse(res.data)["displayName"];
+}
+
+/*
+* @brief gets the description of the user
+* @return description
+*/
+std::string RoPP::User::GetDescription()
+{
+    Request req("https://users.roblox.com/v1/users/" + std::to_string(this->UID));
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    return json::parse(res.data)["description"];
+}
+
+/*
+* @brief gets the groups of the user
+* @return groups json object
+*/
+json RoPP::User::GetGroups()
+{
+    Request req("https://groups.roblox.com/v1/users/" + std::to_string(this->UID) + "/groups/roles");
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    return json::parse(res.data);
+}
+
+/*
+* @brief gets the count of groups the user is in
+* @return count of groups
+*/
+int RoPP::User::GetGroupsCount()
+{
+    Request req("https://groups.roblox.com/v1/users/" + std::to_string(this->UID) + "/groups/roles");
+    req.set_header("Referer", "https://www.roblox.com/");
+    req.initalize();
+    Response res = req.get();
+
+    //Count the number of groups by counting the occurences of "group" in the string
+    std::string word = "group";
+    int count = 0;
+    for (size_t pos = res.data.find(word); pos != std::string::npos; pos = res.data.find(word, pos + word.length()))
+    {
+        ++count;
+    }
+
+    return count;
+}
