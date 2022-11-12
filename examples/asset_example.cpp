d18b67e60bc7ca64a5d4cce8a2f2dc10a1bb3ac9
sightem <sightem@national.shitposting.agency>
2022-11-12 11:21:27 -0500

    asset_example adaptation

--- examples/asset_example.cpp
+++ examples/asset_example.cpp
@@ -1,24 +1,33 @@
 #include "../include/json.hpp"
 #include "../RoPP/ropp.h"
+#include "../include/responses.h"
 #include <iostream>
 #include <string>
 
 
 int main()
 {
-    RoPP::Asset asset(22546563);
+    RoPP::Asset asset(2409285794);
 
-    std::cout << "Asset Name: " << asset.GetName() << std::endl;
-    std::cout << "Asset Description: " << asset.GetDescription() << std::endl;
-    std::cout << "Asset Creator: " << asset.GetCreatorName() << std::endl;
-    std::cout << "Asset Creator ID: " << asset.GetCreatorID() << std::endl;
-    std::cout << "Asset Price: " << asset.GetPrice() << std::endl;
-    std::cout << "Asset Sales: " << asset.GetSales() << std::endl;
-    std::cout << "Asset Is New: " << asset.IsNew() << std::endl;
-    std::cout << "Asset Is Limited: " << asset.IsLimited() << std::endl;
-    std::cout << "Asset Is Limited Unique: " << asset.IsLimitedUnique() << std::endl;
-    std::cout << "Asset Is For Sale: " << asset.IsForSale() << std::endl;
-    std::cout << "Asset Creation Date: " << asset.GetCreationDate() << std::endl;
-    std::cout << "Asset Updated Date: " << asset.GetUpdatedDate() << std::endl;
-    std::cout << "Asset Type: " << asset.GetType() << std::endl;
+    Responses::AssetInfo AssetResponse = asset.GetAssetInfo();
+    Responses::ResaleData ResaleResponse = asset.GetResaleData();
+    
+    std::cout << "Asset Name: " << AssetResponse.Name << std::endl;
+    std::cout << "Asset Description: " << AssetResponse.Description << std::endl;
+    std::cout << "Asset Creator: " << AssetResponse.Creator.Username << std::endl;
+    std::cout << "Asset Creator ID: " << AssetResponse.Creator.UID << std::endl;
+    std::cout << "Asset Asset Type: " << AssetResponse.AssetType << std::endl;
+    std::cout << "Asset Product ID: " << AssetResponse.ProductID << std::endl;
+    std::cout << "Asset Price: " << AssetResponse.Price << std::endl;
+    std::cout << "Asset Sales: " << AssetResponse.Sales << std::endl;
+    std::cout << "Asset Is New: " << AssetResponse.IsNew << std::endl;
+    std::cout << "Asset Is Limited: " << AssetResponse.IsLimited << std::endl;
+    std::cout << "Asset Is Limited Unique: " << AssetResponse.IsLimitedUnique << std::endl;
+    std::cout << "Asset Is For Sale: " << AssetResponse.IsForSale << std::endl;
+    std::cout << "Asset Creation Date: " << AssetResponse.Created.ToISO8601() << std::endl;
+    std::cout << "Asset Updated Date: " << AssetResponse.Updated.ToISO8601() << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Recent Average Resale Price: " << ResaleResponse.RecentAveragePrice << std::endl;
 }
