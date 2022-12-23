d516856bee0e8aff0a15379af39d0e61f795fda7
sightem <sightem@national.shitposting.agency>
2022-12-22 22:28:18 -0500

    group example update

--- examples/group_example.cpp
+++ examples/group_example.cpp
@@ -1,4 +1,4 @@
-#include "../RoPP/ropp.h"
+#include "../include/ropp.h"
 #include "../include/request.hpp"
 #include <iostream>
 #include <string>
@@ -7,16 +7,23 @@ int main()
 {
     RoPP::Group group(1103278);
 
-    std::cout << "Group Name: " << group.GetGroupName() << std::endl;
-    std::cout << "Group Description: " << group.GetGroupDescription() << std::endl;
-    std::cout << "Group Shout: " << group.GetGroupShout() << std::endl;
-    std::cout << "Group Member Count: " << group.GetMemberCount() << std::endl;
-    std::cout << "Group Owner ID: " << group.GetOwnerID() << std::endl;
-    std::cout << "Group Owner Name: " << group.GetOwnerName() << std::endl;
-    std::cout << "Group Owner Display Name: " << group.GetOwnerDisplayName() << std::endl;
-    std::cout << "Group Name History: " << group.GetNameHistory() << std::endl;
-    std::cout << "Group Wall: " << group.GetGroupWall() << std::endl;
-    std::cout << "Group Games: " << group.GetGames() << std::endl;
-    std::cout << "Is Public Entry Allowed: " << group.IsPublicEntryAllowed() << std::endl;
-    std::cout << "Is Builders Club Only: " << group.IsBuildersClubOnly() << std::endl;
+    Responses::Group groupResponse = group.GetGroupInfo();
+
+    std::cout << "Group Name: " << groupResponse.Name << std::endl;
+    std::cout << "Group Description: " << groupResponse.Description << std::endl;
+    std::cout << "Group Shout: " << groupResponse.Shout.Body << std::endl;
+    std::cout << "Group Member Count: " << groupResponse.MemberCount << std::endl;
+    std::cout << "Group Owner ID: " << groupResponse.Owner.UID << std::endl;
+    std::cout << "Group Owner Name: " << groupResponse.Owner.Username << std::endl;
+    std::cout << "Group Owner Display Name: " << groupResponse.Owner.DisplayName << std::endl;
+    std::cout << "Is Public Entry Allowed: " << groupResponse.IsPublicEntryAllowed << std::endl;
+    std::cout << "Is Builders Club Only: " << groupResponse.IsBuildersClubOnly << std::endl;
+    
+    std::cout << "Group Wall: ";
+    Responses::GroupWallResponse GroupWall = group.GetGroupWall();
+
+    for (auto post : GroupWall.Posts)
+    {
+         std::cout << post.Body << " (" << post.Created.ToISO8601() << ")" << std::endl;
+    }
 }
